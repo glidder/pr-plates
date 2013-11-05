0892a258ad70e52a8b622a1d47b7f0f8c4882f16
Javier Osuna Herrera <jabieozuna@gmail.com>
2013-11-05 18:33:37 +0100

    Quintana no sabe como cocinar penes

--- Trabajo/ObtenerMatricula_MPGL.m
+++ Trabajo/ObtenerMatricula_MPGL.m
@@ -12,23 +12,30 @@ B = rgb2gray(A);
 % h2 = -1 * ones(3)/9;  h2(2,2) = 8/9;
 % h3 = [-1 -2 -1; -2 12 -2; -1 -2 -1]/16;
 h = butter(20,0.2,'high'); 
-F = filter2(h, B);
-figure, imshow(F, [])
-
+Fa = filter2(h, B);
+figure, imshow(Fa, [])
+F=Fa(:,10:length(Fa) - 10);
 sf = sum(F); 
 sf = sf - min(sf); sf = sf / max(sf);
 figure, plot(sf)
 
 [sfo, sfoi] = sort(sf, 'descend');
 sfoil = [];
-for i = 3 : length(sfoi)
-    if sfoi(i) >= (sfoi(3) * 0.85)
-        sfoil(i) = sfoi(i);
+aux = 1;
+for i = 1 : length(sfoi)
+    if sfo(i) >= (sfo(1) * 0.65)
+        sfoil(aux) = sfoi(i);
+        aux = aux + 1;
     end
 end
+
+%Temporal version QuintanaAlfa
+
+[minY, minI] = min(sfoil, [], 2);
+[maxY, maxI] = max(sfoil, [], 2);
 %mu_sf = mean(sfoi(3:6))
 %RF = B(:, mu_sf-100:mu_sf+100);
-RF = B(:, min(sfoil), max(sfoil));
+RF = B(:, minY(1):maxY(1));
 figure, imshow(RF, [])
 
 FC = F(:, mu_sf-100:mu_sf+100);
