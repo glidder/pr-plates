6714342d2802a3f65319f779fb623dd747be7ae9
glidder <faceless.goblin@gmail.com>
2014-01-02 10:38:16 +0100

    whatever

--- Trabajo/Programa/LectordeMatriculas.m
+++ Trabajo/Programa/LectordeMatriculas.m
@@ -1,14 +1,19 @@
 	clear all
     clc
+    A=0;
     for i = 1 : 172
     	cd ..
     	cd RPtrain
         I = imread(['Coche' num2str(i,'%03d') '.jpg']);
         cd ..
         cd Programa
-        M = obtenerMatricula(I);
+        [M, T] = obtenerMatricula(I);
         figure, imshow(M);
-        pause;
+        if T==true
+        	A=A+1;
+        end
+        disp([num2str(A) '/' num2str(i) '=' num2str(A*100/i)]); 
+        %pause;
         close all;
     end
     
